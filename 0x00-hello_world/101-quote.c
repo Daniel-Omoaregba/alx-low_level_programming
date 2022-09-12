9d6e0dac23729711d99ad3770ce9faaeb4ee4f41
Daniel <eromoseleomoaregba@gmail.com>
2022-09-11 23:37:21 -0700

    this is an advance project

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: this holds every other functons
+ * Return: 0
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
