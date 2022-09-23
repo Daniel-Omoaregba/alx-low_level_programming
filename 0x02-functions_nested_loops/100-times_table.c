bd4363f1d3d46e643e35207a2d44be0dba059416
Daniel <eromoseleomoaregba@gmail.com>
2022-09-23 13:02:22 -0700

    This is a project

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,44 +1,47 @@
-#include <stdio.h>
-/**
-* print_to_98 - function
-*
-* @n: parameter
-*
-* Return: natural numbers
-*/
-void print_to_98(int n)
-{
-	int upper = 98;
+#include "main.h"
 
-	if (n > upper)
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (n >= 0 && n <= 15)
 	{
-		while (n >= upper)
+		for (i = 0; i <= n; i++)
 		{
-			if (n != upper)
+			for (j = 0; j <= n; j++)
 			{
-				printf("%d%s", n, ", ");
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
-			else
-			{
-				printf("%d", n);
-			}
-			n--;
+			_putchar('\n');
 		}
 	}
-	else
-	{
-		while (n <= upper)
-		{
-			if (n != upper)
-			{
-				printf("%d%s", n, ", ");
-			}
-			else
-			{
-				printf("%d", n);
-			}
-			n++;
-		}
-	}
-	putchar(10);
 }
