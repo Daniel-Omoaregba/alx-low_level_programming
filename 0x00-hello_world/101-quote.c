26f7caa30d078d340deb34da693b6b7d33c85add
Daniel <eromoseleomoaregba@gmail.com>
2022-09-23 13:18:17 -0700

    This is a project

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - main function
- * Description: this holds every other functons
- * Return: 0
+ * main - print quote
+ *
+ * Description: prints Dors's quote
+ *
+ * Return: Always 1 (Success)
  */
+
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
