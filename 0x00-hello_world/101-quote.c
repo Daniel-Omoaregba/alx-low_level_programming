3aa3ad44aa1b4e163656ac592da9fd512c9185c1
Daniel Omoaregba <eromoseleomoaregba@gmail.com>
2023-05-18 15:38:58 -0700

    added advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+/**
+ * main - standard error
+ * @void: no arg
+ *
+ * Description:print to standard error
+ * Return: 1
+ */
+#include <stdio.h>
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
