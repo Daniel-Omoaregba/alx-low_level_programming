66c07894770ac9091c46076e0294b5be1c7b4398
Daniel Omoaregba <eromoseleomoaregba@gmail.com>
2023-05-24 18:18:40 +0100

    added the mandatory tasks 9 to 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * @x: first operand
+ * @y: second operand
+ *
+ * Return: x + y
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * times_table - print 9 times table
+ */
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			if (z / 10 > 0)
+			{
+				_putchar(z / 10 + '0');
+			}
+			_putchar(z % 10 + '0');
+			if ( y == 9)
+			{
+				continue;
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
