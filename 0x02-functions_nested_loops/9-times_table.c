fff2afd393af3c5bb9eb4de8ff385e2421291e3b
Daniel Omoaregba <eromoseleomoaregba@gmail.com>
2023-05-25 11:55:02 +0100

    worked task 9
    it was not outputting the right result because of the _putchar used. it was changed to printf (which ALX did not state can be used)

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,37 +1,22 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * times_table - print 9 times table
  */
 void times_table(void)
 {
-	int x, y, z;
+	int x, y;
 
 	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
-			z = x * y;
-			if (z / 10 > 0)
+			printf("%2d", x * y);
+			if (y < 9)
 			{
-				_putchar(z / 10 + '0');
-				_putchar(z % 10 + '0');
-				if (y < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			else
-			{
-				_putchar(z % 10 + '0');
-				if (y < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
+				printf(", ");
 			}
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
