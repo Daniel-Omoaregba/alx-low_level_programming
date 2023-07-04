835e9bed4eeab95ec57a7abf0023914716d3c0b7
Daniel Omoaregba <eromoseleomoaregba@gmail.com>
2023-07-04 19:17:02 +0100

    worked on advance task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,11 @@
+
 #include <stdio.h>
 /**
  * newwork - function that prints string before main function is executed
  */
-void newwork (void) __attribute__((constructor));
-void newwork (void)
+void newwork(void) __attribute__((constructor));
+void newwork(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
