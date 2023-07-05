b1206bba334e4e4b41f91aa340972a0712c9068e
Daniel Omoaregba <eromoseleomoaregba@gmail.com>
2023-07-05 19:29:43 +0100

    worked on task 4 and 5

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,40 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - creates a new dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *
+ * Return: NULL if function fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+	char *name_cpy;
+	char *owner_cpy;
+	int len1, len2;
+
+	len1 = strlen(name);
+	len2 = strlen(owner);
+	new = malloc(sizeof(dog_t));
+	if (new == NULL || name == NULL || owner == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	name_cpy = malloc(len1 + 1);
+	owner_cpy = malloc(len2 + 1);
+	if (name_cpy == NULL || owner_cpy == NULL)
+	{
+		free(name_cpy);
+		free(owner_cpy);
+		return (NULL);
+	}
+	strcpy(name_cpy, name);
+	strcpy(owner_cpy, owner);
+	new->name = name_cpy;
+	new->owner = owner_cpy;
+	new->age = age;
+	return (new);
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs.
+ * @d: pointer to structure.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	else
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
