6a5e54b353a9761db5d03474fe920e241f8ce6c5
Daniel Omoaregba <eromoseleomoaregba@gmail.com>
2023-07-16 12:17:58 +0100

    worked on this task

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -15,7 +15,7 @@ void reverse_array(int *a, int n)
 	for (i = 0, j = (n - 1); i < n / 2; i++, j--)
 	{
 		tmp = *(a + i);
-	  	*(a + i) = * (a + j);
+		*(a + i) = *(a + j);
 		*(a + j) = tmp;
-	}	       
+	}
 }
