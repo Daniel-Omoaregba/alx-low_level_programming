dce575370515f5ad01fb30324eb83ef7595ee459
Daniel Omoaregba <eromoseleomoaregba@gmail.com>
2023-07-16 11:54:23 +0100

    worked on this task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,31 +1,23 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * rev_string - reverses a string
- * @s: arg that holds the parameter s
+ * @s: points to the string s
  */
 void rev_string(char *s)
 {
-	int i, j, n, m;
+	int num_string, i, j;
+	char tmp;
 
-	n = 0;
+	num_string = 0;
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		printf("%d  %c\n", i, *(s + i));
-		n++;
+		num_string++;
 	}
-	printf("%d\n", n);
-
-	for (j = 0, m = n - ; j <= n; j++)
+	for (i = 0, j = num_string - 1; i < (num_string - 1) / 2; i++, j--)
 	{
-		if (j < n)
-		{
-			*(s + j) = *(s + m);
-			printf("%c\n", *(s + j));
-			m--;
-		}
-		else
-		{
-			*(s + j) = '\0';
-		}
+		tmp = *(s + i);
+		*(s + i) = *(s + j);
+		*(s + j) = tmp;
 	}
 }
